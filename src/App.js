d08bae63e13e144a87262dcee0f5c7942ea3bce1
Milu Franz <mfranz@Milus-MacBook-Pro.local>
2019-07-31 08:30:48 -0400

    Add form stuff

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ function App() {
   return (
     <div className="App">
       <form onSubmit={addTodo}>
+        <h1>React TO-DO List</h1>
+        <label>Enter a to-do:</label>
         <input value={input} onChange={changeInput} />
+        <button>Submit</button>
       </form>
     </div>
   );
