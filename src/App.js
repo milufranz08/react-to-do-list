a4782b7ab5be2d42014c1b1af3cfd3ed736f1a08
Milu Franz <mfranz@Milus-MacBook-Pro.local>
2019-07-31 08:01:37 -0400

    change function names

--- src/App.js
+++ src/App.js
@@ -6,29 +6,29 @@ function App() {
   const [input, setInput] = useState("");
   const [list, setList] = useState([]);
 
-  const onChange = e => {
+  const changeInput = e => {
     setInput(e.target.value);
   };
 
-  const onSubmit = e => {
+  const addTodo = e => {
     e.preventDefault();
     setList([...list, input]);
     setInput("");
   };
 
-  const removeItem = index => {
+  const deleteTodo = index => {
     setList([...list.slice(0, index), ...list.slice(index + 1)]);
   };
 
   return (
     <div className="App">
-      <form onSubmit={onSubmit}>
+      <form onSubmit={addTodo}>
         <h1>React TO-DO List</h1>
         <label>Enter a to-do:</label>
-        <input value={input} onChange={onChange} />
+        <input value={input} onChange={changeInput} />
         <button>Submit</button>
       </form>
-      <List list={list} removeItem={removeItem} />
+      <List list={list} removeItem={deleteTodo} />
     </div>
   );
 }
